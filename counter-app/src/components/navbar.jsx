85b480f85e9b25ff30ee72f9f4484e1d95510689
Cy Sembrano <cyrus.sembrano@gmail.com>
2019-08-03 20:47:18 +1200

    Update navbar.jsx

--- counter-app/src/components/navbar.jsx
+++ counter-app/src/components/navbar.jsx
@@ -1,9 +1,9 @@
-import React, { Component } from "react";
+import React from "react";
 //Stateless Functional Component
 const NavBar = ({ totalCounters }) => {
   return (
     <nav className="navbar navbar-light bg-light">
-      <a className="navbar-brand" href="#">
+      <a className="navbar-brand" href="http://www.google.com">
         NavBar{" "}
         <span className="badge badge-pill badge-secondary">
           {totalCounters}
